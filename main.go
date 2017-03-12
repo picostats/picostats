79707e2a241cade5e0451857f0e7d22396dedeeb
Tihomir Piha <piha.tihomir@gmail.com>
2017-03-12 01:02:35 +0100

    configured server and installed letslencrypt

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-func main() {
-    
-}
+import (
+    "gopkg.in/kataras/iris.v6"
+    "gopkg.in/kataras/iris.v6/adaptors/httprouter"
+)
 
+func main() {
+    app := iris.New()
+    app.Adapt(httprouter.New())
+
+    app.Get("/", func(ctx *iris.Context) {
+        ctx.Writef("Hi %s", "iris")
+    })
+
+    app.Listen(":8080")
+}
