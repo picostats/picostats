c5a1fed7c3ed45f9d464c5e7a5044a29b84ab12b
Tihomir Piha <piha.tihomir@gmail.com>
2017-03-12 02:11:39 +0100

    config - first version

--- config.go
+++ config.go
@@ -1,34 +1,40 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "os"
+	"encoding/json"
+	"log"
+	"os"
 )
 
 // config represents a config object
 type config struct {
-    DBUrl   string `json:"db_url"`
-    AppUrl  string `json:"app_url"`
+	DBUrl    string `json:"db_url"`
+	AppUrl   string `json:"app_url"`
+	RedisUrl string `json:"redis_url"`
 }
 
 // Load method loads config file in config object
 func (c *config) load(configFile string) error {
-    file, err := os.Open(configFile)
+	file, err := os.Open(configFile)
 
-    if err != nil {
-        log.Printf("[config.go] Error while opening config file: %v", err)
-        return err
-    }
+	if err != nil {
+		log.Printf("[config.go] Error while opening config file: %v", err)
+		return err
+	}
 
-    decoder := json.NewDecoder(file)
+	decoder := json.NewDecoder(file)
 
-    err = decoder.Decode(&c)
+	err = decoder.Decode(&c)
 
-    if err != nil {
-        log.Printf("[config.go] Error while decoding JSON: %v", err)
-        return err
-    }
+	if err != nil {
+		log.Printf("[config.go] Error while decoding JSON: %v", err)
+		return err
+	}
 
-    return nil
+	return nil
+}
+
+func initConfig() {
+	conf = &config{}
+	conf.load("config.json")
 }
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+    "log"
+
     "gopkg.in/kataras/iris.v6"
     "gopkg.in/kataras/iris.v6/adaptors/httprouter"
 )
 
+var conf *config
+
 func main() {
     app := iris.New()
     app.Adapt(httprouter.New())
+
+    initConfig()
+
+    log.Println(conf.RedisUrl)
 
     app.Get("/", home)
 
