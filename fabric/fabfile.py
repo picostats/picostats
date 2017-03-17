546cd9ff04d761787861a0f4fc3cd3351c8b98ee
Tihomir Piha <piha.tihomir@gmail.com>
2017-03-17 12:18:33 +0100

    make a deployment script

---
+++ fabric/fabfile.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from fabric.api import env, local, run, sudo
+from fabric.operations import put
+
+env.hosts = ['picostats@46.101.254.59']
+
+
+def deploy():
+    local('mkdir picostatsnew')
+    local('cp ../picostats picostatsnew/')
+    local('cp -R ../public picostatsnew/')
+    local('cp -R ../templates picostatsnew/')
+    local('cp production.json picostatsnew/config.json')
+    local('tar -czf picostatsnew.tar.gz picostatsnew/')
+    local('rm -rf picostatsnew')
+    put('picostatsnew.tar.gz')
+    run('tar -xzf picostatsnew.tar.gz')
+    run('rm -rf picostats')
+    run('mv picostatsnew picostats')
+    local('rm picostatsnew.tar.gz')
+    sudo('supervisorctl reload')
